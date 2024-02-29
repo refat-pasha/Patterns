d802e319e4e208a3b3b3dc189324d120f76fe00b
turtle <141565698+refat-pasha@users.noreply.github.com>
2024-02-29 23:30:55 +0600

    Right Side Triangle.py

--- Right Side Triangle.py
+++ Right Side Triangle.py
@@ -2,7 +2,7 @@
 
 
 
-"""#n = int(input()) #if user want to take input
+#n = int(input()) #if user want to take input
 n = 5 #if input preset
 for i in range(n):
     for j in range(i, n):
@@ -10,13 +10,6 @@ for i in range(n):
     for j in range(i+1):
         print('*',end=' ')
     print()
-"""
-def right_upward_triangle(n):
-    for i in range(1, n + 1):
-        print(" " * (n - i) + "*" * i)
-
-# Example usage:
-right_upward_triangle(5)
 
 
 
